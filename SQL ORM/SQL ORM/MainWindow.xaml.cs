156e29d54c48be8216fb4fa0ab8e3f829fb0838a
Rau-36 <raul363636r36d@gmail.com>
2023-10-05 17:34:50 +0400

    start

---
+++ SQL ORM/SQL ORM/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SQL_ORM;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public object ActiveViewModel { get; set; }
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ SQL ORM/SQL ORM/ViewModels/ViewModels Base/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQL_ORM.ViewModels.ViewModels_Base;
+
+abstract class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void PropertyChangeMethod<T>(out T field, T value, [CallerMemberName] string propName = "")
+    {
+        field = value;
+
+        if (this.PropertyChanged != null)
+        {
+            this.PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
