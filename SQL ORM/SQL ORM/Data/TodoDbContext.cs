212df35f4266d63ba501f868225bedf84162a1ff
Rau-36 <raul363636r36d@gmail.com>
2023-10-10 12:39:19 +0400

    add DbContext

---
+++ SQL ORM/SQL ORM/Data/TodoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SQL_ORM.Data
+{
+    public class TodoDbContext : DbContext
+        {
+            private const string connectionString = "Server=localhost;Database=Db;TrustServerCertificate=True"; //User Id=admin;Password=admin;
+
+
+        public DbSet<Task> Tasks { get; set; }
+            
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(connectionString);
+
+                base.OnConfiguring(optionsBuilder);
+            }
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+            }
+        }
+    }
+
